eb18d04fc903d4dd3cfba2dcf49d8c3127aae1d6
BADHON <physics1233217001@gmail.com>
2026-07-28 21:41:01 +0600

    add git Escape

---
+++ Escape.c
@@ -0,0 +1,10 @@
+// use escape function in c programming language like: /n, /t, /r, /\, /", etc.
+#include <stdio.h>
+
+int main() {
+printf("My name is Badhon, I am a C programmer.\n");// output: My name is Badhon, I am a C programmer.
+printf("My name is Badhon, \nI am a C programmer.");// output: My name is Badhon, 
+// I am a C programmer. 1st \n is used to break the line and 2nd \n is used to break the line too.      
+// try. it dude
+    return 0;
+}
