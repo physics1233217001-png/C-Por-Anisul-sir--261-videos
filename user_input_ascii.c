f8b671e5e7275f051658b1489ebb45df0ee2d3ef
BADHON <physics1233217001@gmail.com>
2026-08-01 21:42:23 +0600

    char to ascii

---
+++ user_input_ascii.c
@@ -0,0 +1,12 @@
+// This program takes a character input from the user and prints its corresponding ASCII value.
+#include <stdio.h>
+
+int main() {
+char c ;
+printf(" Enter char to get equal Ascii Value:");
+scanf("%c" , &c);
+printf("The equal Ascii Value is %d",c);
+    return 0;
+    // you can also use the following format specifiers to read and print different types of variables:
+    // %f for float, %lf for double, %c for char, %s for string, %d for integer, %u for unsigned integer, %x for hexadecimal, %
+}
