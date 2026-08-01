d4cebb7155db8d9d71595d65eea04ab85d625cab
BADHON <physics1233217001@gmail.com>
2026-08-01 21:00:29 +0600

    Add sizeof.c program to demonstrate sizeof operator usage and end it

---
+++ sizeof.c
@@ -0,0 +1,15 @@
+// This program demonstrates the use of the sizeof operator in C to determine the size of different data types.
+#include <stdio.h>
+
+int main() {
+int a;
+float b;
+char c;
+double d;
+printf(" the size of int %d \n", sizeof(a));
+printf(" the size of float %d \n", sizeof(b));
+printf(" the size of char %d \n", sizeof(c));
+printf(" the size of double %d \n", sizeof(d));
+//you must use %d format specifier to print the size of data types in bytes, as sizeof returns a value of type size_t, which is an unsigned integer type. The %d format specifier is used to print integer values in C.
+    return 0;
+}
