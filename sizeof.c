6962d0425ad5bead2d97014f4080619ab6152fe4
BADHON <physics1233217001@gmail.com>
2026-08-01 21:03:17 +0600

    Update sizeof.c to include 'bytes' in size output for clarity

--- sizeof.c
+++ sizeof.c
@@ -6,10 +6,10 @@ int a;
 float b;
 char c;
 double d;
-printf(" the size of int %d \n", sizeof(a));
-printf(" the size of float %d \n", sizeof(b));
-printf(" the size of char %d \n", sizeof(c));
-printf(" the size of double %d \n", sizeof(d));
+printf(" the size of int %d bytes\n", sizeof(a));
+printf(" the size of float %d bytes\n", sizeof(b));
+printf(" the size of char %d bytes\n", sizeof(c));
+printf(" the size of double %d bytes\n", sizeof(d));
 //you must use %d format specifier to print the size of data types in bytes, as sizeof returns a value of type size_t, which is an unsigned integer type. The %d format specifier is used to print integer values in C.
     return 0;
 }
