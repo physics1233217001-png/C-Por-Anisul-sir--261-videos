6429224afb1e0eeb30ea8576b53febc0def38107
BADHON <physics1233217001@gmail.com>
2026-08-12 03:03:44 +0600

    Clarify comment on type casting in average calculation

--- Arithmetic_Operator_Average_sum(5.34).c
+++ Arithmetic_Operator_Average_sum(5.34).c
@@ -7,7 +7,7 @@ printf("Enter two Numbers:");
 scanf("%d %d", &num1,&num2);
 sum = num1+num2;
 average = sum/2.0;
-average = (float)sum/2; // type castings
+average = (float)sum/2; // type castings, those are same as above line
 printf(" The sum is %d ",sum);
 printf(" The average is %.2f ",average);
     return 0;
