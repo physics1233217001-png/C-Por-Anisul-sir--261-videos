591b3aeb812290f1500b170150879d0e58c96ab7
BADHON <physics1233217001@gmail.com>
2026-08-12 03:01:00 +0600

    Fix average calculation and improve output formatting in sum and average program

--- Arithmetic_Operator_Average_sum(5.34).c
+++ Arithmetic_Operator_Average_sum(5.34).c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-int num1,num2,sum,average;
+int num1,num2,sum;
+float average;
 printf("Enter two Numbers:");
 scanf("%d %d", &num1,&num2);
 sum = num1+num2;
-average = sum/2;
+average = sum/2.0;
 printf(" The sum is %d ",sum);
-printf(" The average is %d ",average);
+printf(" The average is %.2f ",average);
     return 0;
 }
