29fd28e1d511a2c7cfd3f8211815a0dc3abc03c1
BADHON <physics1233217001@gmail.com>
2026-08-12 03:03:02 +0600

    Fix average calculation by adding type casting for accurate results

--- Arithmetic_Operator_Average_sum(5.34).c
+++ Arithmetic_Operator_Average_sum(5.34).c
@@ -7,6 +7,7 @@ printf("Enter two Numbers:");
 scanf("%d %d", &num1,&num2);
 sum = num1+num2;
 average = sum/2.0;
+average = (float)sum/2; // type castings
 printf(" The sum is %d ",sum);
 printf(" The average is %.2f ",average);
     return 0;
