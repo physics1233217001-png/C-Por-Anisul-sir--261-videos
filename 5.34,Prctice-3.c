e545eea6590ccaf4fd3c45b1ed7c5fc42f24b3ed
BADHON <physics1233217001@gmail.com>
2026-08-12 19:30:53 +0600

    Add calculation for remainder in the sum, average, and multiplication program

--- 5.34,Prctice-3.c
+++ 5.34,Prctice-3.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 int main() {
-int num1,num2,num3,sum, multiplication;
+int num1,num2,num3,sum,remainder, multiplication;
 printf("Enter three numbers:");
 scanf("%d %d %d ",&num1,&num2,&num3);
 float avg;
 sum = (num1+num2+num3);
 avg = sum/3.0;
 multiplication = num1*num2*num3;
+remainder = num1%num2;
 printf("Sum is %d",sum);
 printf("Average is %.2f",avg);
 printf("Multiplication is %d",multiplication);  
+printf("Remainder is %d",remainder);
 
     return 0;
 }
