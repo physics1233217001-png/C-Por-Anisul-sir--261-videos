db98e09d5d58641953244bb2a22f433d90562e20
BADHON <physics1233217001@gmail.com>
2026-08-12 21:59:42 +0600

    Fix input prompts and improve area output formatting in triangle calculation program

--- 5.34,practice-4.c
+++ 5.34,practice-4.c
@@ -3,11 +3,11 @@
 
 int main() {
 float base,hight,area;
-printf("Enter base  of triangle:\n");
-scanf("%f ", &base);
-printf("Enter hight of triangle:\n");
+printf("Enter base  of triangle:");
+scanf("%f", &base);
+printf("Enter hight of triangle:");
 scanf("%f", &hight);
 area = .5*base*hight;
-printf(" The area of triangle is %f", area);
+printf(" The area of triangle is %.2f", area);
     return 0;
 }
