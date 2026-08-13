a51097bdf20c3b1c23413a8cc38b7963a6affe10
BADHON <physics1233217001@gmail.com>
2026-08-13 13:27:57 +0600

    Add program to swap two numbers

---
+++ 5.51,practice,swap_two numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+int num1 = 5 ;
+int num2 = 10 ;
+int temp;
+temp = num1;
+num1 = num2;
+num2 = temp;
+printf("The value of num1 is now %d\n",num1);
+printf("The value of num2 is now %d",num2);
+
+    return 0;
+}
