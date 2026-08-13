ad8f72e4cbbc4430b489a4c712bd2d5d993e6816
BADHON <physics1233217001@gmail.com>
2026-08-13 15:33:49 +0600

    Add program to calculate absolute value using abs function

---
+++ 5.57,absolute_value.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h> // you must include #include <stdlib.h> header file for abs function
+
+int main() {
+int result = abs(-25);
+printf("Result is %d",result);
+
+    return 0;
+}
