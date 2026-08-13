45713b05025ac11cf99efa8f00301140f8409d52
BADHON <physics1233217001@gmail.com>
2026-08-13 12:03:13 +0600

    Add program to calculate the area of a rectangle using length and width

---
+++ 5.44,practice,arear_of_rectanc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+float leanth,width,area;
+printf(" Enter leanth: ");
+scanf("%f",&leanth);
+printf(" Enter Width: ");
+scanf("%f",&width);
+area = leanth*width;
+printf("Area is%.2f",area);
+
+    return 0;
+}
