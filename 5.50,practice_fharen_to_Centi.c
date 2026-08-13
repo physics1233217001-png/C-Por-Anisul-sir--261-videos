1ef6e946407cffae5c43562af28c73ce3733ad62
BADHON <physics1233217001@gmail.com>
2026-08-13 12:47:05 +0600

    Add program to convert Fahrenheit to Centigrade

---
+++ 5.50,practice_fharen_to_Centi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+float cen,fah;
+printf("Enter Fharenheit:");
+scanf("%f",&fah);
+cen = ((fah-32)*5)/9;
+printf("Centigrade  is %.2f\n",cen);
+
+    return 0;
+    return 0;
+}
