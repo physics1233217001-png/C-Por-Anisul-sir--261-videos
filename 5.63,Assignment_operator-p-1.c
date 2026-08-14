59f3d501e74a72aee819178463a14842836554c6
BADHON <physics1233217001@gmail.com>
2026-08-14 20:14:59 +0600

    Add program to demonstrate assignment operators in C

---
+++ 5.63,Assignment_operator-p-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int a = 5;
+a += 2; // a = a+2
+printf("%d\n",a);
+a *= 2; // a = a*2
+printf("%d\n",a);
+float b = 5;
+b /= 2; // a = a/2
+printf("%f\n",b);
+ 
+
+    return 0;
+}
