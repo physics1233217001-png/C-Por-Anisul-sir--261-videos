0ebec3f5eaf1457a9a2f4d07d4914fbd316981b0
BADHON <physics1233217001@gmail.com>
2026-08-14 21:48:05 +0600

    Fix formatting of output statements in unary increment/decrement operator demonstration

--- 5.64,unary_(pre-post)(in-de)crement_operator.c
+++ 5.64,unary_(pre-post)(in-de)crement_operator.c
@@ -2,12 +2,12 @@
 
 int main() {
 int x =10;
-printf(" %d\n", x++);
-printf(" %d\n", x);
-printf(" %d\n", ++x);
-printf(" %d\n", x);
-printf(" %d\n", x--);
-printf(" %d\n", x);
-printf(" %d\n", --x);
+printf(" %d\n", x++);// x= 10
+printf(" %d\n", x);// x= 11
+printf(" %d\n", ++x);// x= 12
+printf(" %d\n", x);// x= 12
+printf(" %d\n", x--);// x= 12
+printf(" %d\n", x);// x= 11
+printf(" %d\n", --x);// x= 10
     return 0;
 }
