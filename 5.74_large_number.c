87a2188049b4fc5799d777d27095ad661332a893
BADHON <physics1233217001@gmail.com>
2026-09-01 16:26:02 +0600

    Add program to find the larger of two numbers

---
+++ 5.74_large_number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+int num1,num2;
+printf("Enter 2 numbers");
+scanf("%d %d",&num1,&num2);
+if(num1>num2)
+printf("Large =%d",num1);
+else if(num1>num2)
+printf("Large =%d",num2);
+else
+printf("Numbers are equal");
+
+
+
+    return 0;
+}
